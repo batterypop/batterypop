c30f60310b192da13ddda38d8206454ba31df607
grimmwerks <grimm@grimmwerks.com>
2013-07-25 12:12:18 -0400

    initial commit of bpopv2

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the Rails application.
+require File.expand_path('../application', __FILE__)
+
+# Initialize the Rails application.
+Batterypopv2::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Batterypopv2::Application.config.secret_key_base = '722508112f10a587fdc057647aedf1574f79ddb6207dcb05466e8e92a670c57146d197b5389123dac5d8e113b6fb3e8d2862937abfd6eafc16d192acb51836fd'
