f42ee362a1a653689855f8ae7871c4e379b18732
grimmwerks <grimm@grimmwerks.com>
2013-09-18 02:42:48 -0400

    vendor compiled assets

---
+++ public/assets/ckeditor/plugins/stylescombo/lang/sv-e897d7d1fe1a0b7fb9c15955ef53795e.js
@@ -0,0 +1,12 @@
+/*
+Copyright (c) 2003-2013, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+
+CKEDITOR.plugins.setLang( 'stylescombo', 'sv', {
+	label: 'Anpassad stil',
+	panelTitle: 'Formatmallar',
+	panelTitle1: 'Blockstil',
+	panelTitle2: 'Inbäddad stil',
+	panelTitle3: 'Objektets stil'
+});
