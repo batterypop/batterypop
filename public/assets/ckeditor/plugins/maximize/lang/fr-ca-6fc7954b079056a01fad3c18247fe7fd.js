32532dd92284ff0cc7241125305677f9d05175b6
grimmwerks <grimm@grimmwerks.com>
2013-09-18 14:01:16 -0400

    heroku dev says to precompile before push; tryin again

---
+++ public/assets/ckeditor/plugins/maximize/lang/fr-ca-6fc7954b079056a01fad3c18247fe7fd.js
@@ -0,0 +1,1 @@
+CKEDITOR.plugins.setLang("maximize","fr-ca",{maximize:"Maximize",minimize:"Minimize"});
---
+++ public/assets/ckeditor/plugins/maximize/lang/gl-9531d5625806d9e03585200c6d45380b.js
@@ -0,0 +1,1 @@
+CKEDITOR.plugins.setLang("maximize","gl",{maximize:"Maximize",minimize:"Minimize"});
