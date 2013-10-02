631cc19a55efc1631b4bd333164b8a70263603ca
grimmwerks <grimm@grimmwerks.com>
2013-10-02 02:21:16 -0400

    adding static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,20 +9,44 @@ class PagesController < ApplicationController
 
   def contact
     @title = "Contact Us"
+     @active = "contact"
   end
   
   def about
     @title = "About Us"
+     @active = "about"
   end
   
   def shows
     @title = "Shows"
+     @active = "shows"
   end
 
   def shorts
     @title = "Shorts"
+     @active = "shorts"
     @shorts = Show.where(:single => true, :approved => true)
     @groups = @shorts.each_slice(6).to_a
+  end
+
+  def creators
+    @title = "Calling All Creators"
+     @active = "creators"
+  end
+
+  def privacy
+    @title = "Privacy Policy"
+     @active = "privacy"
+  end
+
+  def terms
+    @title = "Terms and Conditions"
+    @active = "terms"
+  end
+
+  def advertise
+    @title = "Advertise on batteryPOP!"
+     @active = "advertise"
   end
 
 end
