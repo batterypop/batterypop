1b2b1ce7060539b6428c14fd85c290e0505eea4e
grimmwerks <grimm@grimmwerks.com>
2013-10-19 17:06:00 -0400

    signing in users redirect to whatever page they were viewing, not just root

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,33 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  after_filter :store_location
+
 
   def most_popped
     #@most_popped  ||= Show.first
     @most_popped ||= "This is a test"
   end
 helper_method :most_popped
+
+
+    
+
+  def store_location
+    # store last url - this is needed for post-login redirect to whatever the user last visited.
+    if (request.fullpath != "/users/sign_in" &&
+        request.fullpath != "/users/sign_up" &&
+        request.fullpath != "/users/password" &&
+        !request.xhr?) # don't store ajax calls
+      session[:previous_url] = request.fullpath 
+    end
+  end
+
+  def after_sign_in_path_for(resource)
+    session[:previous_url] || root_path
+  end
+
+
 
 
   protected
