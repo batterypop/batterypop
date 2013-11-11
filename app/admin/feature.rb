a61af5f5c20469f27f3c44a20a8a30b3d67140ff
grimmwerks <grimm@grimmwerks.com>
2013-11-11 03:44:13 -0500

    changing features and channels

--- app/admin/feature.rb
+++ app/admin/feature.rb
@@ -1,12 +1,20 @@
 ActiveAdmin.register Feature do
+	config.sort_order = "position_asc"
 	menu :parent => "BatteryPOP Home"
 
 	form do |f|
 		f.inputs "BatteryPOP Feature" do
+			f.input :position, :label => "Slide Position"
 			f.input :title,  :required => true 
 			f.input :content,  :as => :rich, :allow_embeds => true
 		end
 		f.buttons
+	end
+
+	index do
+		column :position
+		column :title
+		column :content
 	end
 
 end
