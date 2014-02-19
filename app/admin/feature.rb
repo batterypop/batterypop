492d8870f671320aa189d6050ca672db462ebd6b
grimmwerks <grimm@grimmwerks.com>
2014-02-18 22:58:11 -0500

    changes to sponsor pages, backend, css, models

--- app/admin/feature.rb
+++ app/admin/feature.rb
@@ -20,14 +20,21 @@ ActiveAdmin.register Feature do
 	index :as => :block do |resource|
 
 		links = ''.html_safe
-				links += link_to I18n.t('active_admin.view'), resource_path(resource), :class => "member_link show_link"
-				links += link_to I18n.t('active_admin.edit'), edit_resource_path(resource), :class => "member_link edit_link"
-				links += link_to I18n.t('active_admin.delete'), resource_path(resource), :method => :delete, :confirm => I18n.t('active_admin.delete_confirmation'), :class => "member_link delete_link"
-				
+		links += link_to I18n.t('active_admin.view'), resource_path(resource), :class => "member_link show_link"
+		links += link_to I18n.t('active_admin.edit'), edit_resource_path(resource), :class => "member_link edit_link"
+		links += link_to I18n.t('active_admin.delete'), resource_path(resource), :method => :delete, :confirm => I18n.t('active_admin.delete_confirmation'), :class => "member_link delete_link"
+
+
+		url = ''.html_safe
+		if resource.owner.nil?
+			url = "<a href='/'>View Home Page</a>"
+		else
+			url = link_to resource.owner.title, edit_admin_friend_path(resource.owner)
+		end
 
 		panel "#{resource.position}   #{resource.title}   <div style='float: right;'>#{links}</div>".html_safe do
 			div :for => resource do
-				raw(resource.content)
+				raw(resource.content + '<br />' + url)
 			end
 		end
 	end
--- app/admin/friend.rb
+++ app/admin/friend.rb
@@ -13,6 +13,8 @@ ActiveAdmin.register Friend do
     f.inputs "Sponsor Details" do
       f.input :title, :required => true
       f.input :sponsor, :required => true
+      f.input :approved, :required => true
+      f.input :primary_color, :label => "Main Color:", :hint => "In hexidecimal (ie #ffffff) or 'red'."
       f.input :description,  :label => "Description", :as => :rich, :allow_embeds => true
       f.input :image,  :hint => f.object.image.present? \
         ? f.template.image_tag(f.object.image.url(:thumb))
@@ -36,7 +38,9 @@ ActiveAdmin.register Friend do
     f.actions
   end
 
-
+  # action_item  do
+  #   link_to('View on site', friend_path(friend)) 
+  # end
 
 
   index do
@@ -46,7 +50,15 @@ ActiveAdmin.register Friend do
       link_to image_tag(creator.image(:thumb)), admin_creator_path(creator)
     end
     column :features_autoplay
-    default_actions
+    # default_actions
+    column "" do |resource|
+      links = ''.html_safe
+      links += link_to I18n.t('active_admin.view'), resource_path(resource), :class => "member_link show_link"
+      links += link_to I18n.t('active_admin.edit'), edit_resource_path(resource), :class => "member_link edit_link"
+      links += link_to I18n.t('active_admin.delete'), resource_path(resource), :method => :delete, :confirm => I18n.t('active_admin.delete_confirmation'), :class => "member_link delete_link"
+      links += link_to 'Site View', friend_path(resource)
+      links
+    end
   end
 
 
