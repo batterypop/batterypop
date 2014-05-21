027b17f35506098dea458aa4a230f256744d770d
grimmwerks <grimm@grimmwerks.com>
2014-05-21 01:02:44 -0400

    changes to config for cache store

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -41,7 +41,7 @@ Batterypopv2::Application.configure do
   config.log_level = :debug
 
   # Use a different cache store in production.
-  # config.cache_store = :mem_cache_store
+  config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = "http://assets.example.com"
