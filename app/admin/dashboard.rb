a58bdcb7c32b443075f6c61a974964a4c1410653
grimmwerks <grimm@grimmwerks.com>
2014-06-13 01:55:56 -0400

    adding dashboard changes

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -5,21 +5,25 @@ ActiveAdmin.register_page "Dashboard" do
   content :title => proc{ I18n.t("active_admin.dashboard") } do
     
 
-    
+    thisday =  Date.today.strftime("%Y-%m-%d")
 
     columns do
       column do
-        panel "Top Daily Visits" do
+        panel "Top Daily Visits - #{thisday}" do
             con = ActiveRecord::Base.connection()
 
-            sql = "select v.link_id, l.linkedmedia_id as episode_id,  e.title as episode,  count(*) as count from visits as v, links as l, episodes as e  where v.created_at::TEXT like '2014-06-09%' and (v.link_id = l.id) and (e.id = l.linkedmedia_id)  group by e.title, v.link_id, l.id order by count desc limit 10;"
+            sql = "select v.link_id, l.linkedmedia_id as episode_id,  e.title as episode,  count(*) as count from visits as v, links as l, episodes as e  where v.created_at::TEXT like '#{thisday}%' and (v.link_id = l.id) and (e.id = l.linkedmedia_id)  group by e.title, v.link_id, l.id order by count desc limit 10;"
 
             result = con.execute(sql)
-            
-            table_for result do |item|
-                column("Episode") {|item| link_to(item['episode'], edit_admin_episode_path(Episode.find(item['episode_id']))) } 
-                column("Visits"){ |item| item['count'] } 
-            end
+           
+            if (result.count != 0)
+                table_for result do |item|
+                    column("Episode") {|item| link_to(item['episode'], edit_admin_episode_path(Episode.find(item['episode_id']))) } 
+                    column("Visits"){ |item| item['count'] } 
+                end
+            elsif 
+                h4 "No results found."
+            end   
         end
       end
 
