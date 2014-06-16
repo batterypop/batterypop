4f2fcf1efe8a399a59faf3ec8a3e498c9d8642e4
grimmwerks <grimm@grimmwerks.com>
2014-06-16 02:10:19 -0400

    changes to dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -18,11 +18,10 @@ ActiveAdmin.register_page "Dashboard" do
             sql = "select v.link_id, l.linkedmedia_id as episode_id,  e.title as episode,  count(*) as count from visits as v, links as l, episodes as e  where v.created_at::TEXT like '#{thisday}%' and (v.link_id = l.id) and (e.id = l.linkedmedia_id)  group by e.title, v.link_id, l.id order by count desc limit 10;"
 
             result = con.execute(sql)
-           h4 'result ' + result.count.to_s
+           
             if (result.count != 0)
                 table_for result do |item|
-                    # column("Episode") {|item| link_to(item['episode'], edit_admin_episode_path(Episode.find(item['episode_id']))) } 
-                    column("Episode") {|item|  item['episode']  } 
+                    column("Episode") {|item| link_to(item['episode'], edit_admin_episode_path(Episode.find(item['episode_id']))) } 
                     column("Visits"){ |item| item['count'] } 
                 end
             elsif 
