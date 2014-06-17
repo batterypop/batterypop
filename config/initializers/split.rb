0212768197cc792b3e045542e08499e71347b3c3
grimmwerks <grimm@grimmwerks.com>
2014-06-17 13:25:56 -0400

    fixing redis

--- config/initializers/split.rb
+++ config/initializers/split.rb
@@ -1,6 +1,6 @@
 Split.configure do |config|
 	config.enabled = !Rails.env.test?
-	# Split.redis = REDIS
+	Split.redis = REDIS
 	# Split::Dashboard.use Rack::Auth::Basic do |username, password|
 	# 	username == 'username' && password == 'password'
 	# end
