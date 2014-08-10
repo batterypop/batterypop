25fc466484af156aefb5aed0d65797a03f49c9f4
grimmwerks <grimm@grimmwerks.com>
2014-08-10 04:16:32 -0400

    javascript to pick up external links and create modal

---
+++ app/assets/javascripts/link_modal_redirect.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+	$('a[href]').not('a[href*="batterypop.com"], a[href="#"], a[href^="/"], a[href="https://plus.google.com/111613426507812683994"]').each(function(){
+	    var url = $(this).attr('href');
+		// alert(url);
+		$(this).attr("data-toggle", "modal");
+		$(this).attr("data-target", "#modalLinkJump");
+		$(this).attr("data-url", url);
+		$(this).click(function() {
+		  $('#btnYes').attr('data-url', url);
+		  $('#modalLinkJump').find('.modal-body').html("<h2>You're about leave us to visit another website: <br /><small>" + url + ".</small></h2><p>Are you sure you want to leave batteryPOP? (You can always come back!)</p>");
+		  $(this).preventDefault();
+		});
+		// $(this).preventDefault();
+	});
+
+	$('#btnYes').click(function(){
+		window.location.href = $(this).attr("data-url");
+	});
+
+});
