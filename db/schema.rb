e976f5c901530bd28b5ea18f76a8fe9b65e96495
grimmwerks <grimm@grimmwerks.com>
2014-09-04 22:39:48 -0400

    pushing schema to force change in staging

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema.define(version: 20140902133125) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "pg_stat_statements"
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
