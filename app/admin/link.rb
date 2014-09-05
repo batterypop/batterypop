7330839af88af7b1980db2e65360a11bc464a7bc
grimmwerks <grimm@grimmwerks.com>
2014-09-05 01:28:22 -0400

    attempting de-collection of visits in links as uneeded; might make join on visit side, just testing

--- app/admin/link.rb
+++ app/admin/link.rb
@@ -1,6 +1,16 @@
 ActiveAdmin.register Link do
   menu :parent => "bPOP Pages"
 
+  controller do
+    # def scoped_collection
+    #     Link.where(link_type: 'url')
+    # end
+    # def collection
+    #   puts ""; puts " "; puts "%%%*******%%%%%%%%%%%%%%%%****************%%%%%%%%%%%%************"; puts "";
+     
+    # end
+  end
+
   form do |f|
     f.inputs "Links" do
       f.input :title
@@ -17,18 +27,13 @@ ActiveAdmin.register Link do
     f.actions
   end
 
-  
-  # See permitted parameters documentation:
-  # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # permit_params :list, :of, :attributes, :on, :model
-  #
-  # or
-  #
-  # permit_params do
-  #  permitted = [:permitted, :attributes]
-  #  permitted << :other if resource.something?
-  #  permitted
-  # end
-  
+
+  index do
+    column :title
+    column :url
+    column :link_type
+    column :created_at
+    default_actions
+  end
+
 end
--- app/models/link.rb
+++ app/models/link.rb
@@ -3,7 +3,7 @@ class Link < ActiveRecord::Base
 
 	# default_scope order('created_at DESC')
 
-	has_many :visits
+	# has_many :visits
 	has_many :sidebars
 
 
